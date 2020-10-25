8455f6de8c6b9e648d06ab3943dc27f3463da93d
ukiyoo-home <ukiyoo@example.com>
2020-10-25 22:45:35 +0400

    Implemented the server side of the application using django and rest api
    Django uses two classes TodoList and TodoDetail as well as get functions,
    post to get a list of tasks and add a new task.
    TodoDetail implements getting a specific object and deleting it.

---
+++ backend/API/views.py
@@ -0,0 +1,39 @@
+from API.models import Todo
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
+
+class TodoList(APIView):
+    def get(self, request, format="json"):
+        todo = Todo.objects.all()
+        serializer = TodoSerializer(todo, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format="json"):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TodoDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise Http404
+    
+    def get(self, requset, pk, format='json'):
+        todo = self.get_object(pk)
+        serializers = TodoSerializer(todo)
+        return Response(serializers.data)
+
+    def delete(self, request, pk, fromat="json"):
+        todo = self.get_object(pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
